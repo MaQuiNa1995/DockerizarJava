85ba821c8b68851a5ed7b1bb1b3b39f08942b3f1
MaQuiNa1995 <maquina1995@gmail.com>
2021-09-24 23:42:11 +0200

    Commit inicial con configuración de docker

---
+++ src/main/java/maquina1995/docker/controller/HolaController.java
@@ -0,0 +1,14 @@
+package maquina1995.docker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HolaController {
+
+	@GetMapping
+	public String holaMundo() {
+		return "Hola mundo desde docker";
+	}
+
+}
