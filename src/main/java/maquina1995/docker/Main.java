a84eb0281609506bf6e07b013c867398aa17ea42
MaQuiNa1995 <maquina1995@gmail.com>
2021-09-25 00:16:29 +0200

    Simplificación del ejemplo y banner añadido

--- src/main/java/maquina1995/docker/Main.java
+++ src/main/java/maquina1995/docker/Main.java
@@ -1,13 +1,26 @@
 package maquina1995.docker;
 
+import java.util.logging.Logger;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Main {
 
+	private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
 	public static void main(String[] args) {
-		SpringApplication.run(Main.class, args);
+		SpringApplication.run(Main.class);
+	}
+
+	@GetMapping
+	public String holaMundo() {
+		LOGGER.info("Se ha entrado al controller");
+		return "Hola mundo desde docker";
 	}
 
 }
